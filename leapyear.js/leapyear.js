a5ca0032a59e48c9df3cc351f44b919b83c6c9f3
Suman1228 <suman.t2022ece@sece.ac.in>
2024-02-29 21:42:17 +0530

    Calculator

---
+++ leapyear.js/leapyear.js
@@ -0,0 +1,12 @@
+let a=12
+let b=25
+{
+    if (number % 3 === 0 && number % 5 === 0)
+     {
+        console.log("hi hello");
+    } else if (number % 3 === 0) {
+        console.log("hi");
+    } else if (number % 5 === 0) {
+        console.log("hello");
+    }
+}
